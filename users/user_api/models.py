c8233512e7a8cf813128b350c0f32c1ceec60a1a
Luis Andres <145376515+LuisAndresCarbon@users.noreply.github.com>
2023-12-13 09:14:24 -0600

    ProyectoInicializado

---
+++ users/user_api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
